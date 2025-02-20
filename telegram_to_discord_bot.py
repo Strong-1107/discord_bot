022de6e7b7f44249a262b1f61a295c04d265d116
Strong-1107 <henry.scott.tech@gmail.com>
2025-02-20 00:02:45 -0800

    credential information was created

--- telegram_to_discord_bot.py
+++ telegram_to_discord_bot.py
@@ -5,7 +5,9 @@ import os
 
 load_dotenv()
 
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 
-
-#use the channel username or ID
 
