0a6baf193741532cae85b021a424629d6bd26cdb
Strong-1107 <henry.scott.tech@gmail.com>
2025-02-20 00:05:23 -0800

    telegram client is initialized

--- telegram_to_discord_bot.py
+++ telegram_to_discord_bot.py
@@ -10,4 +10,15 @@ API_HASH = os.getenv('API_HASH')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 
+#define folders
+IMAGE_FOLDER = "images"
+VIDEO_FOLDER = "videos"
+os.makedirs(IMAGE_FOLDER, exist_ok=True)
+os.makedirs(VIDEO_FOLDER, exist_ok=True)
+
+#initialize the telegram client
+telegram_client = TelegramClient('session_name', API_ID, API_HASH)
+
+
+
 
