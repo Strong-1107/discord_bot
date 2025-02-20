67c9c7fd1ac3119da95819c85e3c6940e8eb4bb4
Strong-1107 <henry.scott.tech@gmail.com>
2025-02-19 23:59:30 -0800

    removed bad code

--- telegram_to_discord_bot.py
+++ telegram_to_discord_bot.py
@@ -5,22 +5,7 @@ import os
 
 load_dotenv()
 
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 
-IMAGE_FOLDER = "images"
-VIDEO_FOLDER = "videos"
-os.makedirs(IMAGE_FOLDER, exist_ok=True)
-os.makedirs(VIDEO_FOLDER, exist_ok=True)
-#initialize telegram client
-telegram_client = TelegramClient('session_name', API_ID, API_HASH)
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.messages = True
-discord_client = discord.Client(intents = intents)
 
 #use the channel username or ID
 
